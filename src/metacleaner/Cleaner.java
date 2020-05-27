15aa867d929b90e4388a09b55600ff7828825158
ozzi- <oz@oz-web.com>
2020-05-27 17:16:20 +0200

    .

--- src/metacleaner/Cleaner.java
+++ src/metacleaner/Cleaner.java
@@ -1,6 +1,7 @@
 package metacleaner;
 
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -8,47 +9,48 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
 import org.apache.pdfbox.pdmodel.PDDocumentInformation;
+import org.apache.pdfbox.pdmodel.common.PDMetadata;
 
 public class Cleaner {
-	
+
 	public static void main(String[] args) {
 		// TODO parse args
 		Overwrite ov = new Overwrite(true, "_cleaned");
-		
+
 		try {
-			clean("C:\\Users\\ozzi\\Desktop\\example.pdf",ov);
-		}catch (Exception e) {
+			clean("C:\\Users\\ozzi\\Desktop\\example.pdf", ov);
+		} catch (Exception e) {
 			e.printStackTrace();
 			System.exit(1);
 		}
 	}
-	
+
 	public static void clean(String path, Overwrite overwrite) throws IOException {
 		File file = new File(path);
-		if(!file.exists()) {
-			throw new FileNotFoundException("File '"+path+"' not found or readable");
+		if (!file.exists()) {
+			throw new FileNotFoundException("File '" + path + "' not found or readable");
 		}
-		
+
 		String pathLower = path.toLowerCase();
-		int lastDot = path.lastIndexOf(".")+1;
+		int lastDot = path.lastIndexOf(".") + 1;
 		String fileEnding = pathLower.substring(lastDot);
-		
-		if(fileEnding.equals("jpg") || fileEnding.equals("jpeg")) {
+
+		if (fileEnding.equals("jpg") || fileEnding.equals("jpeg")) {
 			System.out.println("File ending found: JPG");
 			stripImage(path, "jpg", overwrite);
-		}else if(fileEnding.equals("png")) {
+		} else if (fileEnding.equals("png")) {
 			System.out.println("File ending found: PNG");
 			stripImage(path, "png", overwrite);
-		}else if(fileEnding.equals("pdf")) {
+		} else if (fileEnding.equals("pdf")) {
 			System.out.println("File ending found: PDF");
-			stripPDF(path,overwrite);
-		}else {
-			throw new UnsupportedOperationException("File type '"+fileEnding+"' is not implemented");			
+			stripPDF(path, overwrite);
+		} else {
+			throw new UnsupportedOperationException("File type '" + fileEnding + "' is not implemented");
 		}
-		System.out.println("Written clean document to "+overwrite.getPath(path));
+		System.out.println("Written clean document to " + overwrite.getPath(path));
 	}
-	
 
 	private static void stripImage(String path, String outType, Overwrite overwrite) {
 		BufferedImage image;
@@ -62,10 +64,16 @@ public class Cleaner {
 	}
 
 	private static void stripPDF(String path, Overwrite overwrite) throws IOException {
-        PDDocument document = PDDocument.load(new File(path));
-        PDDocumentInformation a = new PDDocumentInformation();
-		document.setDocumentInformation(a);
+		PDDocument document = PDDocument.load(new File(path));
+		// clean basic
+		PDDocumentInformation empty = new PDDocumentInformation();
+		document.setDocumentInformation(empty);
+		// clean XMP
+		PDDocumentCatalog catalog = document.getDocumentCatalog();
+		PDMetadata newMetadata = new PDMetadata(document, new ByteArrayInputStream("".getBytes()));
+		catalog.setMetadata(newMetadata);
+		
+		
 		document.save(overwrite.getPath(path));
 	}
 }
-
