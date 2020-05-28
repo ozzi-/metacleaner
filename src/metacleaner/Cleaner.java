086c7d4b255b5ed9c2487575354fd0fd17a65421
ozzi- <oz@oz-web.com>
2020-05-28 15:48:26 +0200

    xls & doc & xml

--- src/metacleaner/Cleaner.java
+++ src/metacleaner/Cleaner.java
@@ -1,6 +1,7 @@
 package metacleaner;
 
 import java.awt.image.BufferedImage;
+import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -9,12 +10,19 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.net.MalformedURLException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import javax.imageio.ImageIO;
 
@@ -28,27 +36,28 @@ import org.apache.poi.hpsf.DocumentSummaryInformation;
 import org.apache.poi.hpsf.PropertySet;
 import org.apache.poi.hpsf.SummaryInformation;
 import org.apache.poi.ooxml.POIXMLProperties;
+import org.apache.poi.ooxml.POIXMLProperties.ExtendedProperties;
 import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.poifs.filesystem.DirectoryEntry;
 import org.apache.poi.poifs.filesystem.DocumentEntry;
 import org.apache.poi.poifs.filesystem.DocumentInputStream;
 import org.apache.poi.poifs.filesystem.Entry;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
-import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.odftoolkit.simple.*;
 import org.odftoolkit.simple.meta.Meta;
+import org.openxmlformats.schemas.officeDocument.x2006.customProperties.CTProperties;
 
 public class Cleaner {
 
 	public static void main(String[] args) {
 		// TODO parse args
-		Overwrite ov = new Overwrite(true, "_cleaned",true);
-		
+		Overwrite ov = new Overwrite(true, "_cleaned", true);
+
 		try {
-			clean("C:\\Users\\ozzi\\Desktop\\Grobplanung v4.xlsx", ov);
+			clean("C:\\Users\\ozzi\\Desktop\\example.xml", ov);
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.exit(1);
@@ -60,15 +69,12 @@ public class Cleaner {
 		if (!file.exists()) {
 			throw new FileNotFoundException("File '" + path + "' not found or readable");
 		}
-		
+
 		String pathLower = path.toLowerCase();
 		int lastDot = path.lastIndexOf(".") + 1;
 		String fileEnding = pathLower.substring(lastDot);
 
-		// TODO MS Office
-		// TODO ZIP
-		// TODO PSD
-		// TODO XSD
+		// TODO ZIP - https://github.com/KittyHawkCorp/stripzip/ ?
 
 		if (fileEnding.equals("jpg") || fileEnding.equals("jpeg")) {
 			System.out.println("File ending found: JPG");
@@ -91,35 +97,91 @@ public class Cleaner {
 		} else if (fileEnding.equals("xlsx")) {
 			System.out.println("File ending found: XLSX");
 			stripMSDocExcelNew(path, overwrite);
+		} else if (fileEnding.equals("xml")) {
+			System.out.println("File ending found: XML");
+			stripXMLComments(path, overwrite);
 		} else {
 			throw new UnsupportedOperationException("File type '" + fileEnding + "' is not implemented");
 		}
 		System.out.println("Written clean document to " + overwrite.getPath(path));
 	}
-	
-	private static void stripMSDocExcelNew(String path, Overwrite overwrite) throws EncryptedDocumentException, IOException {
+
+	private static void stripXMLComments(String path, Overwrite overwrite) throws IOException {
+		String xml = readLbL(path);
+		xml = xml.replaceAll("(?s)<!--.*?-->", "");
+		Writer out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(overwrite.getPath(path)), StandardCharsets.UTF_8));
+		try {
+			out.write(xml);
+		} finally {
+			out.close();
+		}
+	}
+
+	private static String readLbL(String filePath) {
+		StringBuilder contentBuilder = new StringBuilder();
+		try (Stream<String> stream = Files.lines(Paths.get(filePath), StandardCharsets.UTF_8)) {
+			stream.forEach(s -> contentBuilder.append(s).append("\n"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return contentBuilder.toString();
+	}
+
+	@SuppressWarnings("deprecation")
+	private static void stripMSDocExcelNew(String path, Overwrite overwrite)
+			throws EncryptedDocumentException, IOException {
 		XSSFWorkbook wb = (XSSFWorkbook) WorkbookFactory.create(new File(path));
-		System.out.println(wb.getActiveSheetIndex()+" "+wb.getAllNames().toString());
-        POIXMLProperties props = wb.getProperties();
+		System.out.println(wb.getActiveSheetIndex() + " " + wb.getAllNames().toString());
+		POIXMLProperties props = wb.getProperties();
 
-        POIXMLProperties.CoreProperties coreProp = props.getCoreProperties();
-        props.clea
-        coreProp.setCreator("Thinktibits");
-        coreProp.setDescription("set Metadata using Apache POI / Java");
-        coreProp.setCategory("Programming"); // category
+		POIXMLProperties.CoreProperties coreProp = props.getCoreProperties();
+		coreProp.setCategory("");
+		coreProp.setContentStatus("");
+		coreProp.setContentType("");
+		coreProp.setCreated("");
+		coreProp.setCreator("");
+		coreProp.setDescription("");
+		coreProp.setIdentifier("");
+		coreProp.setKeywords("");
+		coreProp.setLastModifiedByUser("");
+		coreProp.setLastPrinted("");
+		coreProp.setModified("");
+		coreProp.setRevision("");
+		coreProp.setSubjectProperty("");
+		// TODO harshmode excludes
+		coreProp.setTitle("");
 
-        POIXMLProperties.CustomProperties custProp = props.getCustomProperties();
-        custProp.addProperty("Author", "Thinktibits");// String
-        custProp.addProperty("Year", 2014); // Number Property
-        custProp.addProperty("Published", true); // Yes No Property
-        custProp.addProperty("Typist", "tika");
-        FileOutputStream fos = new FileOutputStream(overwrite.getPath(path));
-        wb.write(fos);
-        fos.close();		
+		POIXMLProperties.CustomProperties custProp = props.getCustomProperties();
+
+		CTProperties underlying = custProp.getUnderlyingProperties();
+		for (int i = 0; i < underlying.sizeOfPropertyArray(); i++) {
+			underlying.removeProperty(i);
+			// TODO remove debug once tested
+			System.out.println("debug - removing '" + underlying.getPropertyArray().toString() + "'");
+		}
+		// custProp.addProperty("Author", "test");
+
+		ExtendedProperties extendedProperties = props.getExtendedProperties();
+		extendedProperties.setApplication("");
+		extendedProperties.setAppVersion("");
+		extendedProperties.setCharacters(1);
+		extendedProperties.setCompany("");
+		extendedProperties.setManager("");
+		extendedProperties.setTemplate("");
+		extendedProperties.setTotalTime(0);
+
+		FileOutputStream fos = new FileOutputStream(overwrite.getPath(path));
+		wb.write(fos);
+		fos.close();
 	}
 
 	private static void stripMSDocNew(String path, Overwrite overwrite) throws Exception {
 		XWPFDocument doc = new XWPFDocument(OPCPackage.open(path));
+		POIXMLProperties properties = doc.getProperties();
+		// TODO
+		properties.getCoreProperties();
+		properties.getCustomProperties();
+		properties.getExtendedProperties();
 		System.out.println(doc.getPart());
 		doc.close();
 	}
@@ -129,7 +191,7 @@ public class Cleaner {
 		POIFSFileSystem poifs = new POIFSFileSystem(is);
 		is.close();
 		DirectoryEntry dir = poifs.getRoot();
-		clearEntries(dir,overwrite);
+		clearEntries(dir, overwrite);
 		OutputStream out = new FileOutputStream(overwrite.getPath(path));
 		poifs.writeFilesystem(out);
 		out.close();
@@ -145,63 +207,64 @@ public class Cleaner {
 				Entry entry = dir.getEntry(entryName);
 				if (entry.isDocumentEntry()) {
 					DocumentEntry dsiEntry = (DocumentEntry) dir.getEntry(entry.getName());
-						DocumentInputStream dis = new DocumentInputStream(dsiEntry);
-						PropertySet ps = new PropertySet();
-						// try to set summary information
+					DocumentInputStream dis = new DocumentInputStream(dsiEntry);
+					PropertySet ps = new PropertySet();
+					// try to set summary information
+					try {
+						ps = new PropertySet(dis);
+						SummaryInformation si = null;
 						try {
-							ps = new PropertySet(dis);						
-							SummaryInformation si = null;
-							try {
-								si = new SummaryInformation(ps);								
-							}catch (Exception e) {}
-							if(si!=null) {
-								si.setApplicationName("");
-								System.out.println(si.getAuthor()+"PRE");
-								si.setAuthor("");
-								si.setCreateDateTime(Calendar.getInstance().getTime());
-								si.setEditTime(0);
-								si.setKeywords("");
-								si.setLastAuthor("");
-								si.setLastPrinted(Calendar.getInstance().getTime());
-								si.setLastSaveDateTime(Calendar.getInstance().getTime());
-								si.setOSVersion(0);
-								si.setTemplate("");
-								if(overwrite.isHarshMode()) {
-									si.setSubject("");
-									si.setTitle("");						    	
-								}
-								si.write(dir, entry.getName());
-							}
-						}catch (Exception e) {
-							dis.close();
+							si = new SummaryInformation(ps);
+						} catch (Exception e) {
 						}
-						// try to set document summary information
-						boolean success = false;
-						try {
-							ps = new PropertySet(dis);
-							success = true;
-						}catch (Exception e) {
-							dis.close();
-						}
-						if(ps!=null && success) {
-							dis.close();						    
-							if(ps.isDocumentSummaryInformation()) {
-								dsi = new DocumentSummaryInformation(ps);
-								dsi.setCustomProperties(new CustomProperties());
-								dsi.setApplicationVersion(0);
-								dsi.setCategory("");
-								dsi.setCompany("lmao");
-								dsi.setContentStatus("");
-								dsi.setContentType("");
-								dsi.setDocumentVersion("");
-								if(overwrite.isHarshMode()) {
-									dsi.setLanguage("");									
-								}
-								dsi.setManager("");
-								dsi.setOSVersion(0);
-								dsi.write(dir, entry.getName());
+						if (si != null) {
+							si.setApplicationName("");
+							System.out.println(si.getAuthor() + "PRE");
+							si.setAuthor("");
+							si.setCreateDateTime(Calendar.getInstance().getTime());
+							si.setEditTime(0);
+							si.setKeywords("");
+							si.setLastAuthor("");
+							si.setLastPrinted(Calendar.getInstance().getTime());
+							si.setLastSaveDateTime(Calendar.getInstance().getTime());
+							si.setOSVersion(0);
+							si.setTemplate("");
+							if (overwrite.isHarshMode()) {
+								si.setSubject("");
+								si.setTitle("");
 							}
-						}				
+							si.write(dir, entry.getName());
+						}
+					} catch (Exception e) {
+						dis.close();
+					}
+					// try to set document summary information
+					boolean success = false;
+					try {
+						ps = new PropertySet(dis);
+						success = true;
+					} catch (Exception e) {
+						dis.close();
+					}
+					if (ps != null && success) {
+						dis.close();
+						if (ps.isDocumentSummaryInformation()) {
+							dsi = new DocumentSummaryInformation(ps);
+							dsi.setCustomProperties(new CustomProperties());
+							dsi.setApplicationVersion(0);
+							dsi.setCategory("");
+							dsi.setCompany("lmao");
+							dsi.setContentStatus("");
+							dsi.setContentType("");
+							dsi.setDocumentVersion("");
+							if (overwrite.isHarshMode()) {
+								dsi.setLanguage("");
+							}
+							dsi.setManager("");
+							dsi.setOSVersion(0);
+							dsi.write(dir, entry.getName());
+						}
+					}
 				} else if (entry.isDirectoryEntry()) {
 					DirectoryEntry b = (DirectoryEntry) entry;
 					clearEntries(b, overwrite);
@@ -232,9 +295,9 @@ public class Cleaner {
 		metadata.setLanguage("");
 		metadata.setPrintDate(Calendar.getInstance());
 		metadata.setPrintedBy("");
-		if(overwrite.isHarshMode()) {
+		if (overwrite.isHarshMode()) {
 			metadata.setSubject("");
-			metadata.setTitle("");			
+			metadata.setTitle("");
 		}
 
 		odfDoc.save(overwrite.getPath(path));
